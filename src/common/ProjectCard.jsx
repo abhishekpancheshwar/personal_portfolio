c60302d438343dec0a0322dfa23434f3066c117c
[Abhishek Pancheshwar] <[abhishekpancheswar8@gmail.com]>
2024-12-17 11:02:49 +0530

    changed

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -2,20 +2,38 @@ import PropTypes from 'prop-types'
 
 function ProjectCard({ src, link, h3, p, h4 }) {
   return (
-    <a href={link} target="_blank">
-      <img className="hover" src={src} alt={`${h3} logo`} /> <br /> <br />
-      <h3>{h3}</h3>
-      <p>{p}</p>
-      <p>{h4}</p>
+    <a href={link} target="_blank" rel="noreferrer">
+      <img className="hover" src={src} alt={`${typeof h3 === 'string' ? h3 : 'project'} logo`} /> <br /> <br />
+      <h3>{typeof h3 === 'object' ? h3.props.children : h3}</h3>
+      <div>{typeof p === 'object' ? p.props.children : p}</div>
+      {typeof h4 === 'object' ? (
+        <div>
+          {h4.props.children.filter(child => child.type !== 'p')}
+          {h4.props.children.find(child => child.type === 'p') && 
+            <p>{h4.props.children.find(child => child.type === 'p').props.children}</p>}
+        </div>
+      ) : (
+        <div>{h4}</div>
+      )}
     </a>
   );
 }
+
 ProjectCard.propTypes = {
   src: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
-  h3: PropTypes.element.isRequired,
-  p: PropTypes.element.isRequired,
-  h4: PropTypes.element.isRequired,
+  h3: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object
+  ]).isRequired,
+  p: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object
+  ]).isRequired,
+  h4: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object
+  ]).isRequired,
 };
 
 export default ProjectCard;
